69072393afd2791ba2a019482e502338daf1ba63
realdiegor <diegoreyes150103@gmail.com>
2023-08-30 13:41:32 -0400

    chore: add config files for eslint, prettier and husky

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Options} */
+export default {
+	semi: true,
+	singleQuote: true,
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	// plugins: ['prettier-plugin-tailwindcss'],
+	// tailwindConfig: 'apps/web/tailwind.config.js',
+	// tailwindFunctions: ['tw'],
+};
