77a8d43d861bc2d9108185bf7f1794c7afcf2110
realdiegor <diegoreyes150103@gmail.com>
2023-09-05 22:33:38 -0400

    feat: add button component
    Changed config in jsconfig.json to enable vscode intellisense. Made link-card component props required

---
+++ src/common/button/variants.js
@@ -0,0 +1,14 @@
+const baseStyles =
+	'flex gap-2 py-2.5 px-5 my-5 text-black text-sm rounded-md transition duration-200 disabled:opacity-50';
+
+export const widthStyles = {
+	'max-content': 'w-max',
+	full: 'w-full justify-center',
+};
+
+export const variantStyles = {
+	primary: `${baseStyles} bg-brand-green hover:bg-[#0FDC68]`,
+	secondary: `${baseStyles} bg-brand-yellow hover:bg-[#DCB129]`,
+	terciary: `${baseStyles} bg-white border-2 border-solid border-black hover:bg-[#EEEEEE]`,
+	danger: `${baseStyles} bg-white text-brand-red border-2 border-solid border-brand-red hover:bg-[#FCEFEF]`,
+};
