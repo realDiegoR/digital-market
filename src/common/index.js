e39f7329f35889e7fed81575a640fdeced13872e
Martin-M42 <83964814+Martin-M42@users.noreply.github.com>
2023-09-07 15:07:07 -0300

    feat: add table component (#8)

--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,7 @@
 export * from './link-card';
+export * from './wrapper/';
 export * from './page-title';
 export * from './wrapper';
 export * from './hamburger-button';
 export * from './button';
+export * from './table';
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,5 +1,6 @@
 import { MainLayout } from '@/layouts/main.layout';
 import { RootPage } from './root/root';
+import { Table } from '../common/table';
 
 import {
 	createBrowserRouter,
@@ -8,10 +9,32 @@ import {
 	RouterProvider,
 } from 'react-router-dom';
 
+const info = [
+	{
+		codigo: 1223344,
+		producto: 'manzana pera casa mi mama me mima',
+		precio: 22.3,
+		importe: 3,
+	},
+	{
+		codigo: 897987,
+		producto: 'frutilladddddddddddddddddddddddddddddddddddddddasdasd',
+		precio: 22.3,
+		importe: 3,
+	},
+	{
+		codigo: 897987,
+		producto: 'frutilla',
+		precio: 22.3,
+		importe: 3,
+	},
+];
+
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<MainLayout />}>
 			<Route index element={<RootPage />} />
+			<Route path="/testtabla" element={<Table list={info} withSelect={true} />} />
 		</Route>
 	)
 );
